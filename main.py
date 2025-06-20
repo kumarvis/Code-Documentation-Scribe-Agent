91e07f26dc5c9eb8290bb5d3a2dcae0c2aa1512c
kumarvis <kumarvis@gmail.com>
2025-06-20 05:47:31 +0000

    Adding venv requirement file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def run():
     source_root_path = 'saras-easd-2020-retinanet'
     code_file_type = ['.py']
     python_files_path_lst = list_files_in_directory(source_root_path, code_file_type)
-    model_name = 'qwen3:1.7b'
+    model_name = 'qwen3:0.6b'
     code_analyzer_obj = CodeAnalyzer(model_name)
     code_reviewer_obj = CodeReviewer(model_name)
     for python_file_path in python_files_path_lst:
@@ -19,10 +19,10 @@ def run():
         for key, value in result_dct.items():
             classes_dct = result_dct['classes']
             functions_dct = result_dct['top_level_functions']
-            for func_name, func_txt in functions_dct:
+            for func_name, func_txt in functions_dct.items():
                 code_documentation = code_analyzer_obj.explain_code(func_txt)
                 reviewed_code_documentation = code_reviewer_obj.review_code(func_txt, code_documentation)
-
+                print(reviewed_code_documentation)
 
             print('break')
 
